68fd41bedd76134442379bec2b12e09d74647cfa
ashkan fekri <ashkanfekridev@gmail.com>
2022-04-09 22:41:33 +0430

    fix host not found

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,7 +2,8 @@ let input_link = document.getElementById("input-link");
 let download_link = document.getElementById("download-link");
 let message = document.getElementById("message");
 let btn_download = document.getElementById("btn-download");
-
+let host_id = document.getElementById("host-id");
+let = hosts = ["host1.rj-mw1.com", "host2.rj-mw1.com"];
 
 // Download file by url
 function downloadFile(url) {
@@ -13,30 +14,55 @@ function downloadFile(url) {
     download_link.click();
 }
 
-btn_download.addEventListener('click', () => {
+function makelink(type, file_name, host_id) {  
+    switch (type) {
+        case "mp3s":
+            console.log('mp3s');
+            return "https://"+host_id+"/media/mp3/mp3-320/" + file_name + ".mp3"
+            break;
+        case "podcasts":
+            console.log('mp3s');
+            return  "https://"+host_id+"/media/podcast/mp3-320/" + file_name + ".mp3"
+            break;
+        case "videos":
+            console.log('videos');
+            return  "https://"+host_id+"/media/music_video/hd/" + file_name + ".mp4"
+            break;
+        default:
+            return false
+            // message.innerText = "دانلود نمیشه این فایل!";
+    }
+} 
+
+input_link.addEventListener("keyup", function(event) {
+    // Number 13 is the "Enter" key on the keyboard
+    // console.log('Enter pressed' + event.keyCode);
+
+    if (event.keyCode === 13) {
+
+        console.log('Enter pressed' + event.keyCode);
+        download();
+    }
+  }); 
+
+function download(){
     message.innerText = "";
     let link = input_link.value.split('/');
     let file_name = link[link.length - 1];
     console.log("filename: " + file_name);
+    var download_link = makelink(link[3], file_name, hosts[host_id.value]);
+    console.log("link is= " + download_link);
+    if (download_link) {
+        downloadFile(download_link);
+        input_link.value = "";
 
-    switch (link[3]) {
-        case "mp3s":
-            console.log('mp3s');
-            downloadFile("https://host2.rj-mw1.com/media/mp3/mp3-320/" + file_name + ".mp3")
-            break;
-        case "podcasts":
-            console.log('mp3s');
-            downloadFile("https://host2.rj-mw1.com/media/podcast/mp3-320/" + file_name + ".mp3")
-            break;
-        case "videos":
-            console.log('videos');
-            downloadFile("https://host2.rj-mw1.com/media/music_video/hd/" + file_name + ".mp4")
-            break;
-        default:
-            message.innerText = "دانلود نمیشه این فایل!";
+    } else {   
+        message.innerText = "دانلود نمیشه این فایل!";   
     }
+}
 
-
+btn_download.addEventListener('click', () => {
+download();
 });
 
 
